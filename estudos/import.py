363fc739be696e577392dcf25bd9947ec69ce505
Optivision Licitacao <munhozrufino.programacao@outlook.com>
2022-08-01 16:59:25 -0300

    Novos estudos.

--- estudos/import.py
+++ estudos/import.py
@@ -6,6 +6,7 @@ print(emoji.emojize('Olá Mundo! :globo_mostrando_as_américas:', language='pt')
 #print(emoji.demojize("Python é 🌍", language='pt')) função demojize utilizada para identificar o nome do emoji
 #import global funciona desta forma: import math
 
+import math # Forma 1: importação total - Forma 2: from math import sqrt, floor
 num = int(input('Digite um número:'))
 raiz = math.sqrt(num)
 print('A raiz de {} é igual a {:.2f}'.format(num, math.floor(raiz)))
