0b81043d4e6fd18944754001fd172777c1d66c5e
Munhoz Rufino <munhozrufino.programacao@outlook.com>
2022-08-05 08:44:59 -0300

    Ajustes aula 21.

--- exercicios/utilizando módulos/ex21.py
+++ exercicios/utilizando módulos/ex21.py
@@ -1,7 +1,7 @@
 #Exercício Python 21: Faça um programa em Python que abra e reproduza o áudio de um arquivo MP3.
 
-'''import pygame
+import pygame
 pygame.init()
-pygame.mixer.music.load('audio_mpeg.mp3')
+pygame.mixer.music.load('audiompeg.mp3')
 pygame.mixer.music.play()
-pygame.event.wait()'''
+pygame.event.wait()
